7c22b1c631fee260d19ac2e3281c6a3e103e58c9
fbgkdn2484 <80086225+fbgkdn2484@users.noreply.github.com>
2022-04-28 12:05:03 +0900

    Add files via upload

---
+++ week9/CastTest.java
@@ -0,0 +1,10 @@
+package week9;
+
+public class CastTest {
+	public static void main(String[] args) {
+		SubCls sub = new SubCls(100, 99, 100);
+		System.out.println(sub);
+		
+		
+	}
+}
---
+++ week9/SubCls.java
@@ -0,0 +1,21 @@
+package week9;
+
+public class SubCls extends SupCls {
+	int sum;
+	double avg;
+	String addr;
+	public SubCls() {
+		sum = kor + eng + math;
+	}
+	public SubCls(int kor, int eng, int math) {
+		this.kor = kor; 
+		this.eng = eng;
+		this.math = math;
+		sum = kor + eng + math;
+		avg = sum / 3.0;
+	}
+	@Override
+	public String toString() {  //상속 받은 것을 재정의 하는 것 오버라이딩
+		return "kor=" + kor + ", eng=" + eng + ", math=" + math + ", sum=" + sum + ", avg=" + avg;
+	}
+}
