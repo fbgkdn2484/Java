3cfab029a06abd5b480697070904b7189f708d57
fbgkdn2484 <80086225+fbgkdn2484@users.noreply.github.com>
2022-05-12 12:03:57 +0900

    Add files via upload

---
+++ week11/A.java
@@ -0,0 +1,11 @@
+package week11;
+
+public class A {
+	
+	//void e();
+	
+	void f() {
+		System.out.println("A¿« f()»£√‚");
+	}
+
+}
---
+++ week11/OverrTest.java
@@ -0,0 +1,13 @@
+package week11;
+
+public class OverrTest {
+	
+	public static void main(String[] args) {
+//		A a = new A();
+//		B b = new B();
+//		b.f();
+//		b.f();
+		A c = new B();
+		c.f();
+	}
+}
